40343e6f2d9219b2617033abe578e55770659756
rick <ricksikka1@gmail.com>
2021-07-01 17:35:05 -0400

    Initial Commit

---
+++ SpotifyUIClone/SpotifyUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUICloneApp.swift
+//  SpotifyUIClone
+//
+//  Created by Sabar Ahuja on 2021-07-01.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
